1ad1d48c64dffacc19c0139ae2331e8921dc279f
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 15:49:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,5 +8,5 @@ class Pokemon
   
   def self.find(id_num, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?, id_num").flatten 
-    
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type)
 end
