791ec8c4e31736ecf9acefe85b24e105eee8bd5e
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 15:48:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,5 +3,5 @@ class Pokemon
   attr_accessor :id, :name, :type, :hp, :db 
   
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)")
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?), name, type")
 end
